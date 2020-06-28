9023186275d5a6491fd33491e9ab8bda382f5576
NachoKingP <brian.remite@gmail.com>
2020-06-28 02:39:25 -0400

    Removed a few unnecessary lines of code..

--- app.py
+++ app.py
@@ -62,53 +62,6 @@ def gdp(year):
     return gdp_df.to_json(orient="values")
 
 
-@app.route("/api/v1.0/tobs")
-def tobs():
-    """Return a JSON list of temperature observations (TOBS) for the previous year."""
-    #Retreive max date data from engine into tuple and retreive first element of tuple
-    max_date = session.query(TMS_Table.date).order_by(TMS_Table.date.desc()).first()
-    max_date = max_date[0]
-    
-    #Calculate the date 1 year ago from today
-    year_ago = dt.datetime.strptime(max_date, "%Y-%m-%d") - dt.timedelta(days=366)
-    
-    #Return the observation data
-    results_tobs = session.query(TMS_Table.date, TMS_Table.tobs).filter(TMS_Table.date >= year_ago).all()
-    
-    #Convert data into list for returning
-    tobs_list = list(results_tobs)
-    
-    #Return JSONified data from list
-    return jsonify(results_tobs)
-
-
-@app.route("/api/v1.0/<start>")
-def start(start=None):
-    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date."""
-    
-    #Pull data from engine for dates after start date specified
-    start_data = session.query(TMS_Table.date, func.min(TMS_Table.tobs), func.avg(TMS_Table.tobs), func.max(TMS_Table.tobs)).filter(TMS_Table.date >= start).group_by(TMS_Table.date).all()
-    
-    #Convert data into list for returning
-    start_list=list(start_data)
-    
-    #Return JSONified data from list
-    return jsonify(start_list)
-
-
-@app.route("/api/v1.0/<start>/<end>")
-def start_end(start=None, end=None):
-    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given date range."""
-    
-    #Pull data from engine for dates range specified
-    range_data = session.query(TMS_Table.date, func.min(TMS_Table.tobs), func.avg(TMS_Table.tobs), func.max(TMS_Table.tobs)).filter(TMS_Table.date >= start).filter(TMS_Table.date <= end).group_by(TMS_Table.date).all()
-    
-    #Convert data into list for returning
-    range_list=list(range_data)
-    
-    #Return JSONified data from list
-    return jsonify(range_list)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
