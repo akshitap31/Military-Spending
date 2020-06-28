c68466ad736459752d37018fa0d2790b4d3b3108
NachoKingP <brian.remite@gmail.com>
2020-06-27 21:10:11 -0400

    PArsed data from SQLite into DF, returned values from dropdown.

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import numpy as np
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect
 import datetime as dt
+import pandas as pd
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///military.sqlite", echo=True)
+engine = create_engine("sqlite:///db/military.sqlite", echo=True)
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -37,43 +38,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return (
-        f"Welcome to the Military Spending API!<br/>"
-        f"Available Routes:<br/>"
-        f"To return a JSON list of Total Military Spending, append: /api/v1.0/tms<br/>"
-        f"To return a JSON list of spending as a percentage of GDP, append: /api/v1.0/gdp<br/>"
-    )
+    return render_template("index.html")
+    # return (
+    #     f"Welcome to the Military Spending API!<br/>"
+    #     f"Available Routes:<br/>"
+    #     f"To return a JSON list of Total Military Spending, append: /api/v1.0/tms<br/>"
+    #     f"To return a JSON list of spending as a percentage of GDP, append: /api/v1.0/gdp<br/>"
+    # )
 
 
-@app.route("/api/v1.0/tms")
-def tms():
-    """Return the JSON representation of the tms data."""
-    #Retreive max date data from engine into tuple and retreive first element of tuple
-    max_date = session.query(TMS_Table.date).order_by(TMS_Table.date.desc()).first()
-    max_date = max_date[0]
-    
-    #Calculate the date 1 year ago from today
-    year_ago = dt.datetime.strptime(max_date, "%Y-%m-%d") - dt.timedelta(days=366)
-    
-    #Return the tms and put it into a dictionary for returning
-    results_tms = session.query(TMS_Table.date, TMS_Table.prcp).filter(TMS_Table.date >= year_ago).all()
-    tms_dict = dict(results_tms)
-    
-    #Return JSONified data from dictionary
-    return jsonify(tms_dict)
+@app.route("/tms/<year>")
+def tms(year):
+    query = f"""SELECT name, "{year}" FROM tms ORDER BY 2 DESC LIMIT 10"""
+    conn = engine.connect()
+    tms_df = pd.read_sql(query, conn)
+    return tms_df.to_json(orient="values")
 
-
-@app.route("/api/v1.0/gdp")
-def gdp():
-    """Return a JSON list of gdp from the dataset."""
-    #Pull station data from engine
-    results_gdp = session.query(TMS_Table.GDP_Table).group_by(TMS_Table.GDP_Table).all()
-    
-    #Convert data into list for returning
-    gdp_list = list(np.ravel(results_gdp))
-    
-    #Return JSONified data from list
-    return jsonify(gdp_list)
+@app.route("/gdp/<year>")
+def gdp(year):
+    query = f"""SELECT name, "{year}" FROM gdp ORDER BY 2 DESC LIMIT 10"""
+    conn = engine.connect()
+    gdp_df = pd.read_sql(query, conn)
+    return gdp_df.to_json(orient="values")
 
 
 @app.route("/api/v1.0/tobs")
