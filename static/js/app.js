16347f96b67e0a5f1e234744964779f3db02f266
NachoKingP <brian.remite@gmail.com>
2020-06-30 19:06:30 -0400

    Fixed chart.js hover glitch.

--- static/js/app.js
+++ static/js/app.js
@@ -45,6 +45,11 @@ function chartBar(xData, yData, cat, year) {
     yTitle = "Total Spend in USD (Billions)"
   }
 
+  // var button = document.getElementById("submitButton");
+  //   submitButton.addEventListener("click", function(){
+  //   myChart.destroy();
+  // });
+
   var ctx = document.getElementById('myChart').getContext('2d');
   var myChart = new Chart(ctx, {
       type: 'bar',
@@ -53,21 +58,48 @@ function chartBar(xData, yData, cat, year) {
           datasets: [{
               label: yTitle,
               data: yDataScaled,
-              backgroundColor: 'rgba(255, 99, 132, 0.2)',
-              borderColor: 'rgba(255, 159, 64, 1)',
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
               borderWidth: 1
           }]
-      }//,
-      // options: {
-      //     scales: {
-      //         yAxes: [{
-      //             ticks: {
-      //                 beginAtZero: true
-      //             }
-      //         }]
-      //     }
-      // }
+      },
+      options: {
+          animation: {
+            duration: 2000,
+          },
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
   });
+  var button = document.getElementById("refresh");
+    refresh.addEventListener("click", function(){
+    myChart.destroy();
+});
 }
 
 
@@ -130,6 +162,7 @@ function init() {
     console.log(values);
 
     bar(names,values,cat,year);
+    chartBar(names,values,cat,year);
   });
 }
   
