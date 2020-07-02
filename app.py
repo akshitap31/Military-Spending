962339a8d7665217d12fcd7eba42d592b1101001
NachoKingP <brian.remite@gmail.com>
2020-07-02 19:32:42 -0400

    Fixed app.py sync issue.

--- app.py
+++ app.py
@@ -61,33 +61,7 @@ def gdp(year):
     gdp_df = pd.read_sql(query, conn)
     return gdp_df.to_json(orient="values")
 
-@app.route("/timeline/tms")
-def timeline_data_tms():
-    conn = engine.connect()
-    df = pd.read_sql("SELECT * FROM tms", conn)
-    df=df.fillna(0)
-    my_dict={}
-    for colname in df.columns[2:]:
-            my_dict[colname]= [{"name": row["name"], "value": row[colname]} for index, row in df.iterrows()]
-    for lis in my_dict:
-        my_dict[lis]=sorted(my_dict[lis], key= lambda i:i["value"], reverse=True)
-        for dic in my_dict[lis]:
-            dic["rank"]=my_dict[lis].index(dic)+1
-    return my_dict
 
-@app.route("/timeline/gdp")
-def timeline_data_gdp():
-    conn = engine.connect()
-    df = pd.read_sql("SELECT * FROM gdp", conn)
-    df=df.fillna(0)
-    my_dict={}
-    for colname in df.columns[2:]:
-            my_dict[colname]= [{"name": row["name"], "value": row[colname]} for index, row in df.iterrows()]
-    for lis in my_dict:
-        my_dict[lis]=sorted(my_dict[lis], key= lambda i:i["value"], reverse=True)
-        for dic in my_dict[lis]:
-            dic["rank"]=my_dict[lis].index(dic)+1
-    return my_dict
 
 if __name__ == "__main__":
     app.run(debug=True)
